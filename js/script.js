a5fc98c26a8a391fc39e4f1f763d207e7728428b
jaehun.sim <jeilpkc@gmail.com>
2026-05-21 20:33:48 +0900

    Smaple structure

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Welcome to jeilpkc.github.io!");
+    
+    // Example: You can add a simple alert or a dark-mode toggle here later
+});
